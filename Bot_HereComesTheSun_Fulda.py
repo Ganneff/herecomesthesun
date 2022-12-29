7ecd01bd5b707c729d921067ec036d3ca907e67d
Joerg Jaspert <joerg@debian.org>
2022-12-29 15:52:29 +0100

    Code is GPL, put notice into code

--- Bot_HereComesTheSun_Fulda.py
+++ Bot_HereComesTheSun_Fulda.py
@@ -8,7 +8,22 @@ coding: utf-8
 https://github.com/halcy/Mastodon.py
 https://docs.stormglass.io/#/
 
-Updated 2022-12-29 for Fulda, and some python changes, Joerg Jaspert <joerg@ganneff.de>
+Copyright (C) 2022 Henrik Schönemann
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Modified 2022-12-29 by Joerg Jaspert <joerg@ganneff.de>
 """
 
 import requests
